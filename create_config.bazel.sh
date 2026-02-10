07a32c283bd230e1c75f1fa944049d96725156e5
Borja Lorente <blorente.me@gmail.com>
2026-02-10 15:35:22 +0000

    fix: Make sure config.h doesn't fail if we're not running at the root

--- create_config.bazel.sh
+++ create_config.bazel.sh
@@ -5,11 +5,19 @@ output="${1}"
 
 function write_define() {
   stamp_variable="${1}"
+  default="${2}"
   variable=${stamp_variable#"STABLE_"}
 
-  value=$( cat bazel-out/stable-status.txt | grep "${variable}" | awk '{print $2}')
+  value=$( cat bazel-out/stable-status.txt | grep "${variable}" || true )
+
+  if [[ "${value}" != "" ]]; then
+    value=$(echo "${value}" | awk '{print $2}')
+  else
+    value="${default}"
+  fi
+
   echo "#define ${variable} \"${value}\"" >> "${output}"
 }
 
-write_define "STABLE_SAIREDIS_GIT_REVISION"
-write_define "STABLE_SAI_GIT_REVISION"
+write_define "STABLE_SAIREDIS_GIT_REVISION" "ffffffff"
+write_define "STABLE_SAI_GIT_REVISION" "ffffffff"
